2442bcea05fa319b3700480effbad29d05f4ea50
shingggk <shingokise@gmail.com>
2024-05-29 14:16:37 -0700

    add normal gpt endpoint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import BaseModel
+
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.document_loaders import TextLoader
@@ -7,12 +10,12 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from pydantic_settings import BaseSettings, SettingsConfigDict
 from langchain.chains import create_retrieval_chain
-from pydantic import BaseModel
 
+# FastAPI app initialization
 app = FastAPI()
 
+# CORS Middleware setup
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -21,6 +24,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Define Pydantic models
 class Prompt(BaseModel):
     message: str
 
@@ -29,35 +33,48 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(env_file=".env")
 
 settings = Settings()
+llm = ChatOpenAI(api_key=settings.openai_api_key)
 
+# Root endpoint
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
-llm = ChatOpenAI(api_key=settings.openai_api_key)
-loader = TextLoader("./intern.txt")
-docs = loader.load()
+# GPT endpoint
+@app.post("/gpt")
+def gpt(user_prompt:Prompt):
+    gpt_prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a world class chatbot."),
+        ("user", "{input}")
+    ])
 
-embeddings = OpenAIEmbeddings(api_key=settings.openai_api_key)
+    chain = gpt_prompt | llm 
+    gpt_res = chain.invoke({"input": f"{user_prompt}"})
+    return {"response": gpt_res["answer"]}
 
-text_splitter = RecursiveCharacterTextSplitter()
-documents = text_splitter.split_documents(docs)
-vector = FAISS.from_documents(documents, embeddings)
-
-prompt = ChatPromptTemplate.from_template("""You are a helpful assistant to students who could use internship information from previous students to apply for an internship. Answer the following question based only on the provided context:
-
-<context>
-{context}
-</context>
-
-Question: {input}""")
-
-document_chain = create_stuff_documents_chain(llm, prompt)
-
-retriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
-
+# Intermship endpoint
 @app.post("/intern")
-def main(user_prompt:Prompt):
+def rag(user_prompt:Prompt):
+    loader = TextLoader("./intern.txt")
+    docs = loader.load()
+
+    embeddings = OpenAIEmbeddings(api_key=settings.openai_api_key)
+
+    text_splitter = RecursiveCharacterTextSplitter()
+    documents = text_splitter.split_documents(docs)
+    vector = FAISS.from_documents(documents, embeddings)
+
+    prompt = ChatPromptTemplate.from_template("""You are a helpful assistant to students who could use internship information from previous students to apply for an internship. Answer the following question based only on the provided context:
+
+    <context>
+    {context}
+    </context>
+
+    Question: {input}""")
+
+    document_chain = create_stuff_documents_chain(llm, prompt)
+
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
     response = retrieval_chain.invoke({"input": f"{user_prompt}"})
     return {"response": response["answer"]}
